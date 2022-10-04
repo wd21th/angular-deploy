f7b730da4660c7164c3e82225d8fc39c4c65cf53
Dias <wprogrammist@gmail.com>
2022-10-05 00:46:29 +0600

    my component

---
+++ src/app/dias/dias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dias',
+  templateUrl: './dias.component.html',
+  styleUrls: ['./dias.component.scss']
+})
+export class DiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
