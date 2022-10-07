36a32041099e98a982957c1d811bb33f65f5cd00
Dias <wprogrammist@gmail.com>
2022-10-07 21:48:20 +0600

    dias component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiasComponent } from './dias/dias.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      DiasComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
